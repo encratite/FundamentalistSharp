ca4a762fb1c565dc2295fafed52f8e54f8cb3d66
void <git.email.address@googlemail.com>
2024-03-29 12:12:51 +0100

    Added a reflection test to get rid of the manual feature extraction.

---
+++ Fundamentalist.Test.Reflection/BaseClass.cs
@@ -0,0 +1,11 @@
+﻿namespace Fundamentalist.Test.Reflection
+{
+	internal class BaseClass
+	{
+		public DerivedClass Object1 { get; set; }
+		public DerivedClass Object2 { get; set; }
+		public DerivedClass Object3 { get; set; }
+		public string String1 { get; set; }
+		public int? Int1 { get; set; }
+	}
+}
---
+++ Fundamentalist.Test.Reflection/DerivedClass.cs
@@ -0,0 +1,9 @@
+﻿namespace Fundamentalist.Test.Reflection
+{
+	internal class DerivedClass
+	{
+		public decimal? Decimal1 { get; set; }
+		public decimal? Decimal2 { get; set; }
+		public decimal? Decimal3 { get; set; }
+	}
+}
