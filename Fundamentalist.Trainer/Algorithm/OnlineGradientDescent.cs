e50ebbaa4848f9d8bb991554f040a28a95796dde
void <git.email.address@googlemail.com>
2024-04-16 00:25:14 +0200

    Improved online gradient descent

--- Fundamentalist.Trainer/Algorithm/OnlineGradientDescent.cs
+++ Fundamentalist.Trainer/Algorithm/OnlineGradientDescent.cs
@@ -4,13 +4,24 @@ namespace Fundamentalist.Trainer.Algorithm
 {
 	internal class OnlineGradientDescent : IAlgorithm
 	{
-		public string Name => "Online Gradient Descent";
+		private int _numberOfIterations;
+		private float _learningRate;
+		private float _l2Regularization;
+
+		public string Name => $"Online Gradient Descent ({_numberOfIterations}, {_learningRate}, {_l2Regularization})";
+
+		public OnlineGradientDescent(int numberOfIterations, float learningRate, float l2Regularization)
+		{
+			_numberOfIterations = numberOfIterations;
+			_learningRate = learningRate;
+			_l2Regularization = l2Regularization;
+		}
 
 		public IEstimator<ITransformer> GetEstimator(MLContext mlContext)
 		{
 			IEstimator<ITransformer> estimator =
 				mlContext.Transforms.NormalizeMinMax("Features")
-				.Append(mlContext.Regression.Trainers.OnlineGradientDescent(numberOfIterations: 100));
+				.Append(mlContext.Regression.Trainers.OnlineGradientDescent(numberOfIterations: _numberOfIterations, learningRate: _learningRate, l2Regularization: _l2Regularization));
 			return estimator;
 		}
 	}
